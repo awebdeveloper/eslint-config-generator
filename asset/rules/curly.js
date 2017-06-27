b617bdb04af1303ff17094a679e34febeb3a61fb
Deepak Brahmania <deepak.brahmania@timeinc.com>
2017-06-27 12:10:24 +0530

    removed header tag

--- asset/rules/curly.js
+++ asset/rules/curly.js
@@ -7,9 +7,9 @@ ESLintConfigGenApp.rules["curly"] = [
       {
         hint: "Enforce the use of curly braces in all block statements( if, else, for, while, or do )",
         code: `
-        if (foo) {
-          foo++;
-        }
+        if (foo) <mark>{</mark>
+          foo++;  // <mark>curly braces are mandatory</mark>
+        <mark>}</mark>
         while (true) {
           baz();
           doSomething();
@@ -20,28 +20,28 @@ ESLintConfigGenApp.rules["curly"] = [
         hint: "Enforce the use of curly braces when multiple statements are present in the block & warn when there is only one statement in the block",
         code: `
         if (foo)
-          foo++;   // no need of curly braces [a warning is thrown]
+          foo++;   // <mark>curly braces are required for multiline statements only [a warning is thrown]</mark>
         else
           foo();
 
-        while (true) {    // requires curly braces in case of multiline statements
+        while (true) <mark>{</mark>
           baz();
           doSomething();
-        }`,
+        <mark>}</mark>`,
         value: ["error", "multi", "consistent"]
       },
       {
         hint: "Relax the warning when there is only one statement in the block while still enforcing the use of curly braces for other instances",
         code: `
         if (foo)
-          foo++;   // no need of curly braces
+          foo++;   //<mark> no need of curly braces [no warning thrown]</mark>
         else
           foo();
 
-        while (true) {    // requires curly braces in case of multiline statements
+        while (true) <mark>{</mark>    // requires curly braces in case of multiline statements
           baz();
           doSomething();
-        }`,
+        <mark>}</mark>`,
         value: ["error", "multi-line", "consistent"]
       }
     ]
